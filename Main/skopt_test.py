8d6c0dcb1451a1d04c77bab5665c00eb0163d028
felix <felix.lundh@gmail.com>
2021-03-19 11:18:53 +0100

    added skopt test

---
+++ Main/skopt_test.py
@@ -0,0 +1,75 @@
+print(__doc__)
+
+import numpy as np
+
+np.random.seed(237)
+import matplotlib.pyplot as plt
+from skopt.plots import plot_gaussian_process
+from skopt import gp_minimize
+from skopt.plots import plot_convergence
+
+noise_level = 0.1
+
+
+def f(x, noise_level=noise_level):
+    return np.sin(5 * x[0]) * (1 - np.tanh(x[0] ** 2)) \
+           + np.random.randn() * noise_level
+
+
+# Plot f(x) + contours
+x = np.linspace(-2, 2, 400).reshape(-1, 1)
+fx = [f(x_i, noise_level=0.0) for x_i in x]
+plt.plot(x, fx, "r--", label="True (unknown)")
+plt.fill(np.concatenate([x, x[::-1]]),
+         np.concatenate(([fx_i - 1.9600 * noise_level for fx_i in fx],
+                         [fx_i + 1.9600 * noise_level for fx_i in fx[::-1]])),
+         alpha=.2, fc="r", ec="None")
+plt.legend()
+plt.grid()
+plt.show()
+
+
+res = gp_minimize(f,                  # the function to minimize
+                  [(-2.0, 2.0)],      # the bounds on each dimension of x
+                  acq_func="EI",      # the acquisition function
+                  n_calls=15,         # the number of evaluations of f
+                  n_random_starts=5,  # the number of random initialization points
+                  noise=0.1**2,       # the noise level (optional)
+                  random_state=1234)   # the random seed
+
+plot_convergence(res)
+
+plt.rcParams["figure.figsize"] = (8, 14)
+
+
+def f_wo_noise(x):
+    return f(x, noise_level=0)
+
+for n_iter in range(5):
+    # Plot true function.
+    plt.subplot(5, 2, 2*n_iter+1)
+
+    if n_iter == 0:
+        show_legend = True
+    else:
+        show_legend = False
+
+    ax = plot_gaussian_process(res, n_calls=n_iter,
+                               objective=f_wo_noise,
+                               noise_level=noise_level,
+                               show_legend=show_legend, show_title=False,
+                               show_next_point=False, show_acq_func=False)
+    ax.set_ylabel("")
+    ax.set_xlabel("")
+    # Plot EI(x)
+    plt.subplot(5, 2, 2*n_iter+2)
+    ax = plot_gaussian_process(res, n_calls=n_iter,
+                               show_legend=show_legend, show_title=False,
+                               show_mu=False, show_acq_func=True,
+                               show_observations=False,
+                               show_next_point=True)
+    ax.set_ylabel("")
+    ax.set_xlabel("")
+
+plt.show()
+
