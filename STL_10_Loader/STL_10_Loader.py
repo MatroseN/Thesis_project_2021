ede15f7ed6272f642a29071f49ac740546b7ea0c
felix <felix.lundh@gmail.com>
2021-03-25 14:28:23 +0100

    added STL-10 Loader script (IT IS NOT DONE!!!)

---
+++ STL_10_Loader/STL_10_Loader.py
@@ -0,0 +1,93 @@
+from __future__ import print_function
+
+import sys
+import os, sys, tarfile, errno
+import numpy as np
+import matplotlib.pyplot as plt
+
+if sys.version_info >= (3, 0, 0):
+    import urllib.request as urllib  # ugly but works
+else:
+    import urllib
+
+try:
+    from imageio import imsave
+except:
+    from scipy.misc import imsave
+
+print(sys.version_info)
+
+# image shape
+HEIGHT = 96
+WIDTH = 96
+DEPTH = 3
+
+# size of a single image in bytes
+SIZE = HEIGHT * WIDTH * DEPTH
+
+# path to the directory with the data
+DATA_DIR = '/STL-10/stl10_binary'
+
+# path to the binary train file with image data
+DATA_PATH = '/STL-10/stl10_binary/train_X.bin'
+
+# path to the binary train file with labels
+LABEL_PATH = '/STL-10/stl10_binary/train_y.bin'
+
+
+def read_labels(path_to_labels):
+    """
+    :param path_to_labels: path to the binary file containing labels from the STL-10 dataset
+    :return: an array containing the labels
+    """
+    with open(path_to_labels, 'rb') as f:
+        labels = np.fromfile(f, dtype=np.uint8)
+        return labels
+
+
+def read_all_images(path_to_data):
+    """
+    :param path_to_data: the file containing the binary images from the STL-10 dataset
+    :return: an array containing all the images
+    """
+
+    with open(path_to_data, 'rb') as f:
+        # read whole file in uint8 chunks
+        everything = np.fromfile(f, dtype=np.uint8)
+
+        # We force the data into 3x96x96 chunks, since the
+        # images are stored in "column-major order", meaning
+        # that "the first 96*96 values are the red channel,
+        # the next 96*96 are green, and the last are blue."
+        # The -1 is since the size of the pictures depends
+        # on the input file, and this way numpy determines
+        # the size on its own.
+
+        images = np.reshape(everything, (-1, 3, 96, 96))
+
+        # Now transpose the images into a standard image format
+        # readable by, for example, matplotlib.imshow
+        # You might want to comment this line or reverse the shuffle
+        # if you will use a learning algorithm like CNN, since they like
+        # their channels separated.
+        images = np.transpose(images, (0, 2, 3, 1))
+        return images
+
+
+def read_single_image(image_file):
+    """
+    CAREFUL! - this method uses a file as input instead of the path - so the
+    position of the reader will be remembered outside of context of this method.
+    :param image_file: the open file containing the images
+    :return: a single image
+    """
+    # read a single image, count determines the number of uint8's to read
+    image = np.fromfile(image_file, dtype=np.uint8, count=SIZE)
+    # force into image matrix
+    image = np.reshape(image, (3, 96, 96))
+    # transpose to standard format
+    # You might want to comment this line or reverse the shuffle
+    # if you will use a learning algorithm like CNN, since they like
+    # their channels separated.
+    image = np.transpose(image, (0, 2, 1))
+    return image
